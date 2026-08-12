25dd8956abbb8bca0c05d750650b677f40bf04a3
s2pArjun <s2parjun@gmail.com>
2026-08-12 11:03:27 +0530

    remove base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/world-geography-recall-system-/',
   plugins: [react()],
   server: {
     port: 5173,
